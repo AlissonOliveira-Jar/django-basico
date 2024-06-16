4f51c75bc46032ae849771f4a71c63c8d819d964
Alisson Oliveira <alisson.dev.py@gmail.com>
2024-06-16 05:24:13 -0300

    :sparkles: feat: torna urls do blog dinamicas

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,15 @@ def blog(request):
     
     return render(request, 'blog/index.html', context)
 
+def post(request, id):
+    print('post', id)
+    
+    context = {
+        'posts': posts,
+    }
+    
+    return render(request, 'blog/index.html', context)
+
 def exemplo(request):
     print('exemplo')
     
