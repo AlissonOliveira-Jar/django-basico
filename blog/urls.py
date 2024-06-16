3dbee306d88a4fa50ee6543489fd9cf2925c2dbf
Alisson Oliveira <alisson.dev.py@gmail.com>
2024-06-16 15:37:26 -0300

    :sparkles: feat: post unico e tratamento 404

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'blog'
 
 urlpatterns = [
-    path("post/<int:id>/", views.post, name='post'),
+    path("post/<int:post_id>/", views.post, name='post'),
     path("", views.blog, name='blog'),
     path("exemplo/", views.exemplo, name='exemplo')
 ]
